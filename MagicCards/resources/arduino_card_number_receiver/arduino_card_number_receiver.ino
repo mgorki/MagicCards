437938946ec34841338c5f0cc3be0e2507f0b71f
mgorki <michael.gorki@gmx.de>
2020-09-28 13:20:08 +0200

    Add files via upload

--- MagicCards/resources/arduino_card_number_receiver/arduino_card_number_receiver.ino
+++ MagicCards/resources/arduino_card_number_receiver/arduino_card_number_receiver.ino
@@ -1,15 +1,20 @@
 // For receiving the input (used to define the card in the Magic Cards experiment) from a 4x4 Matrix sent by another Arduino (using 433MHz RF module).
 // Receiver
+//
+//// Include RadioHead Amplitude Shift Keying Library
+//#include <RH_ASK.h>
+//// Include dependant SPI Library 
+//#include <SPI.h> 
 
-// Include RadioHead Amplitude Shift Keying Library
-#include <RH_ASK.h>
-// Include dependant SPI Library 
-#include <SPI.h> 
- 
-// Create Amplitude Shift Keying Object
-RH_ASK rf_driver;
+//// Create Amplitude Shift Keying Object
+//RH_ASK rf_driver;
 
-// For Testing with buttons
+const byte numChars = 32;
+char msg[numChars]; // an array to store the received data
+
+boolean newNumber = false;
+
+// For the buttons
 const int pinButtonRight = 2;
 const int pinButtonLeft = 3;
 
@@ -18,34 +23,79 @@ void setup()
     // For testing with buttons
     pinMode(pinButtonRight, INPUT);
     pinMode(pinButtonLeft, INPUT);
-      
-    // Initialize ASK Object
-    rf_driver.init();
+//      
+//    // Initialize ASK Object
+//    rf_driver.init();
     // Setup Serial Monitor
     Serial.begin(9600);
 }
  
 void loop()
 {
-    // Set buffer to size of expected message
-    uint8_t buf[2];
-    uint8_t buflen = sizeof(buf);
-    // Check if received packet is correct size
-    if (rf_driver.recv(buf, &buflen))
-    {  
-      // Message received
-      Serial.println((char*)buf);         
-    }
+//    // Set buffer to size of expected message
+//    uint8_t buf[2];
+//    uint8_t buflen = sizeof(buf);
+//    // Check if received packet is correct size
+//    if (rf_driver.recv(buf, &buflen))
+//    {  
+//      // Message received
+//      Serial.println((char*)buf);         
+//    }
 
-    // For testing with buttons
+    RecvEndMarkerNewline();
+    passNumber();
+    
+    // For the buttons
     int stateButtonRight = digitalRead(pinButtonRight);
     int stateButtonLeft = digitalRead(pinButtonLeft);
     if(stateButtonRight == 1) 
       {
-      Serial.println("r"); 
+      Serial.write("r");
+      Serial.write('\n'); 
       } 
-    if(stateButtonLeft == 1) 
+    else if(stateButtonLeft == 1) 
       {
-      Serial.println("l"); 
-      } 
+      Serial.write("l");
+      Serial.write('\n'); 
+      }
+    else
+    {
+      Serial.write("00");
+      Serial.write('\n');   
+    }
+}
+
+void RecvEndMarkerNewline() {
+   static byte ndx = 0;
+   char endMarker = '\n';
+   char rc;
+   
+   // if (Serial.available() > 0) {
+             while (Serial.available() > 0 && newNumber == false) {
+   rc = Serial.read();
+  
+   if (rc != endMarker) 
+   {
+   msg[ndx] = rc;
+   ndx++;
+     if (ndx >= numChars) 
+     {
+      ndx = numChars - 1;
+     }
+   }
+   else {
+     msg[ndx] = '\0'; // terminate the string
+     ndx = 0;
+     newNumber = true;
+   }
+   }
+}
+
+void passNumber() {
+ if (newNumber == true) 
+ {
+   Serial.write(msg);
+   Serial.write('\n');
+   newNumber = false;
+ }
 }
